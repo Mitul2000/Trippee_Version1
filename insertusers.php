7d204e8ed5610c230fbc94ea0d019c1acca14c0d
Mitul2000 <patelmitul5053@gmail.com>
2020-11-13 10:44:36 -0500

    updated the signin bug

--- insertusers.php
+++ insertusers.php
@@ -5,27 +5,27 @@ if ($mysqli -> connect_errno) {
   echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
   exit();
 }
+if (!empty($_POST)){
+  echo $_POST["Username"];
+  $username = mysqli_real_escape_string($mysqli, $_POST["Username"]);
+  $firstname = mysqli_real_escape_string($mysqli, $_POST["Firstname"]);
+  $lastname = mysqli_real_escape_string($mysqli, $_POST["Lastname"]);
+  $nickname = mysqli_real_escape_string($mysqli, $_POST["Nickname"]);
+  $dateofBirth = mysqli_real_escape_string($mysqli, $_POST["Dateofbirth"]);
+  $email = mysqli_real_escape_string($mysqli, $_POST["Emailaddress"]);
+  $password = mysqli_real_escape_string($mysqli, $_POST["Password"]);
+
+  
+  
+
 
 $query = "SELECT * FROM users WHERE (username = '$username');";
 $sql_data = $mysqli -> query($query);
 
-if (mysqli_num_rows($sql_data) != 0){ 
-  if (!empty($_POST)){
-    echo $_POST["Username"];
-    $username = mysqli_real_escape_string($mysqli, $_POST["Username"]);
-    $firstname = mysqli_real_escape_string($mysqli, $_POST["Firstname"]);
-    $lastname = mysqli_real_escape_string($mysqli, $_POST["Lastname"]);
-    $nickname = mysqli_real_escape_string($mysqli, $_POST["Nickname"]);
-    $dateofBirth = mysqli_real_escape_string($mysqli, $_POST["Dateofbirth"]);
-    $email = mysqli_real_escape_string($mysqli, $_POST["Emailaddress"]);
-    $password = mysqli_real_escape_string($mysqli, $_POST["Password"]);
-
-    $mysqli -> query("INSERT INTO trippe.users (username, firstname, lastname, nickname, dateofbirth, email, avatar, password) VALUES ('$username', '$firstname', '$lastname','$nickname','$dateofBirth','$email','','$password')");
-    
+if (mysqli_num_rows($sql_data) == 0){ 
+  $mysqli -> query("INSERT INTO trippe.users (username, firstname, lastname, nickname, dateofbirth, email, avatar, password) VALUES ('$username', '$firstname', '$lastname','$nickname','$dateofBirth','$email','','$password')");
     header("Location: login.html");
     die();
-    
-}
 
 }
 else{
@@ -34,10 +34,8 @@ else{
 
 }
 
+}
 
-
-// Print auto-generated id
-echo "New record has id: " . $mysqli -> insert_id;
 
 $mysqli -> close();
 ?>
